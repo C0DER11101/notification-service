3c2ac3a4a3f539fe7324be03e2a71a1881da9c9c
C0DER11101 <javapr0gramm3r@yahoo.com>
2026-09-13 20:54:52 +0530

    in progress...

---
+++ src/main/java/com/jsp/notification_service/controller/MessageController.java
@@ -0,0 +1,9 @@
+package com.jsp.notification_service.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class MessageController {
+}
